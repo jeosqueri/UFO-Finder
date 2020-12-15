5eaffa8ca08faf778e4e8a770b068709cdf9615d
jeosqueri <juliasqueri303@gmail.com>
2020-12-15 15:06:47 -0700

    wrote code to append table of ufo data to webpage

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,13 @@
 // from data.js
 var tableData = data;
+// get reference to table body
+var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+// code to append table of ufo data to webpage & adds new rows of data for each ufo sighting
+data.forEach((ufoInfo) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoInfo).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
