f291b6871dd99a18a5bd367e8eda46635250a0c1
jeosqueri <juliasqueri303@gmail.com>
2020-12-22 12:58:09 -0700

    got code for filter working, able to display array of input value matches in console

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -11,3 +11,39 @@ data.forEach((ufoInfo) => {
         cell.text(value);
     });
 });
+
+// Use a date form in your HTML document and write JavaScript code 
+// that will listen for events and search through the `date/time` column to find rows that match user input.
+// Select the button
+var button = d3.select("#filter-btn");
+// Select the form
+var form = d3.select("#form");
+
+//Create event handlers
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Complete the event handler function to return the form
+function runEnter() {
+    //Prevent page from refreshing
+    d3.event.preventDefault();
+    //Select the input element
+    var inputElement = d3.select("#datetime");
+    //Get the value property of input element
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    // Use the form input to filter the data by datetime
+    //function dateTime(input) {
+        //return input.datetime === inputValue;
+    //};
+
+    //var filtered = tableData.filter(dateTime);
+    //console.log(filtered);
+
+    var matches = tableData.filter(input => input.datetime === inputValue);
+    console.log(matches);
+
+
+
+
+}
